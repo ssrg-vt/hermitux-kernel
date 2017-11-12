e8e65a92fbcd7aba636ee536646ff9910b74bf26
Pierre Olivier <polivier@vt.edu>
2017-11-11 19:48:10 -0500

    hermitux: arc and argv working!

--- usr/tests/hermitux.c
+++ usr/tests/hermitux.c
@@ -33,23 +33,70 @@
 // Linux applications are always located at address 0x400000
 #define tux_start_address	0x400000
 
+
+#define GET_REG( var, reg, size ) asm volatile("mov"size" %%"reg", %0" : "=m" (var) )
+#define GET_REG64( var, reg ) GET_REG( var, reg, "q")
+#define GET_RSP( var ) GET_REG64( var, "rsp" )
+#define SET_RIP_IMM( var ) asm volatile("movq %0, -0x8(%%rsp); jmpq *-0x8(%%rsp)" : : "m" (var) )
+
 extern const size_t tux_entry;
 extern const size_t tux_size;
 
+struct cmdline_t {
+	int argc;
+	char **argv;
+};
+
 int main(int argc, char** argv)
 {
+	unsigned long long int libc_argc = argc -1;
+	int i;
+
 	printf("Hello from HermiTux loader\n\n");
 
 	if (tux_entry >= tux_start_address) {
-		printf("Found linux image at 0x%zx with a size of 0x%zx\n", tux_start_address, tux_size);
+		printf("Found linux image at 0x%zx with a size of 0x%zx\n", 
+				tux_start_address, tux_size);
 		printf("Entry point is located at 0x%zx\n", tux_entry);
 
-		printf("Value of first byte at entry point: 0x%zx\n", (size_t) *((char*) tux_entry));
-	} else fprintf(stderr, "Unable to find a Linux image!!!\n");
+		printf("Value of first byte at entry point: 0x%zx\n", 
+				(size_t) *((char*) tux_entry));
+	} else 
+		fprintf(stderr, "Unable to find a Linux image!!!\n");
 
-	void (*entry)(void) = (void *)tux_entry;
-	printf("jumping\n");
-	entry();
+	/* Befre jumping to the entry point we need to construct the stack with 
+	 * argc, argv, envp, and auxv according to the linux convention. The layout
+	 * shoud be:
+	 * rsp --> [ argc ]       integer, 8 bytes
+	 *         [ argv[0] ]    pointer, 8 bytes
+	 *         [ argv[1] ]    pointer, 8 bytes
+	 *         ...
+	 *         [ argv[argc] ] (NULL) pointer, 8 bytes
+	 *
+	 *         [ envp[0] ]    pointer, 8 bytes
+	 *         [ envp[1] ]    pointer, 8 bytes
+	 *         ...
+	 *         [ envp[N] ]    (NULL) pointer, 8 bytes
+	 *
+	 *         TODO auxv: unclear for now
+	 *
+	 *         Adapted from:
+	 *         http://articles.manugarg.com/aboutelfauxiliaryvectors
+	 */
+
+	/* We need to push the element on the stack in the inverse order they will 
+	 * be read by the C library (i.e. argc in the end) */
+	
+	/* argv */
+	for(i=libc_argc+1;i>0; i--)
+		asm volatile("pushq %0" : : "r" (argv[i]));
+
+	/* argc */
+	asm volatile("pushq %0" : : "r" (libc_argc));
+	
+
+	printf("Jumping to 0x%x\n", tux_entry);
+	asm volatile("jmp *%0" : : "r" (tux_entry));
 
 	return 0;
 }
