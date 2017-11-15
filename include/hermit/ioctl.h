93185dac04f5496aabd881364091c90af4278faa
Pierre Olivier <polivier@vt.edu>
2017-11-15 14:34:57 -0500

    termios ioctl

--- include/hermit/ioctl.h
+++ include/hermit/ioctl.h
@@ -15,6 +15,23 @@ struct winsize {
 
 #define TIOCGWINSZ 0x00005413
 
+/* needed for TCGETS */
+#define NCCS	19
+
+typedef unsigned char cc_t;
+typedef uint32_t tcflag_t;
+
+struct termios {
+	tcflag_t c_iflag;
+	tcflag_t c_oflag;
+	tcflag_t c_cflag;
+	tcflag_t c_lflag;
+	cc_t c_line;
+	cc_t c_cc[NCCS];
+};
+
+#define TCGETS 0x00005401
+
 #ifdef __cplusplus
 }
 #endif
