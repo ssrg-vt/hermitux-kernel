d5a5a70e51cee2babc4124bca3566f453bf77288
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2017-11-23 23:50:29 +0100

    add missing system calls to support IP

--- kernel/syscalls/bind.c
+++ kernel/syscalls/bind.c
@@ -3,8 +3,19 @@
 #include <hermit/logging.h>
 
 int sys_bind(int fd, struct sockaddr *addr, int addrlen) {
+	struct sockaddr_in sa_server;
+	struct in_addr addr_local;
+
+	addr_local.s_addr = INADDR_ANY;
+
+	memset((char *) &sa_server, 0x00, sizeof(sa_server));
+	sa_server.sin_family = AF_INET;
+	sa_server.sin_port = htons(8000);
+	sa_server.sin_addr = addr_local;
+
 #ifndef NO_NET
-	return bind(fd, addr, addrlen);
+	return bind(fd, (struct sockaddr *) &sa_server, sizeof(sa_server));
+	//return bind(fd, addr, addrlen);
 #else
 	LOG_ERROR("Network disabled, cannot process bind syscall!\n");
 	return -ENOSYS;
