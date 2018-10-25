cbffa9c093dbad6b0da0c39476367d963bb77a4e
Pierre Olivier <polivier@vt.edu>
2018-10-25 18:12:02 -0400

    Rever mmap implementation not to map pages on demand

--- include/hermit/mmap_areas.h
+++
@@ -1,8 +0,0 @@
-#ifndef MMAP_AREAS_H
-#define MMAP_AREAS_H
-
-int mmap_area_remove(uint64_t addr);
-int mmap_area_check(uint64_t addr);
-int mmap_areas_init(void);
-
-#endif /* MMAP_AREAS_H */
--- kernel/syscalls/munmap.c
+++ kernel/syscalls/munmap.c
@@ -3,19 +3,18 @@
 #include <asm/page.h>
 #include <hermit/spinlock.h>
 #include <hermit/memory.h>
-#include <hermit/mmap_areas.h>
-
-extern int check_pagetables(size_t vaddr);
 
 int sys_munmap(size_t viraddr, size_t len) {
 	uint32_t npages = PAGE_CEIL(len) >> PAGE_BITS;
 	int i, ret;
+	size_t phyaddr;
 
 	if (BUILTIN_EXPECT(!viraddr, 0))
 		return -EINVAL;
 	if (BUILTIN_EXPECT(!len, 0))
 		return -EINVAL;
 
+	/* Free virtual address space */
 	ret = vma_free((size_t)viraddr-PAGE_SIZE,
 			(size_t)viraddr+(npages+1)*PAGE_SIZE);
 
@@ -24,31 +23,13 @@ int sys_munmap(size_t viraddr, size_t len) {
 		return ret;
 	}
 
-	/* Unmap only the pages that have actually been mapped */
-	for(i=0; i<npages; i++) {
-		size_t page_addr = viraddr + i * PAGE_SIZE;
-		if(check_pagetables(page_addr)) {
-			size_t phys_addr = virt_to_phys(page_addr);
+	phyaddr = virt_to_phys((size_t)viraddr);
+	if (BUILTIN_EXPECT(!phyaddr, 0))
+		return -EFAULT;
 
-			ret = page_unmap(page_addr, 1);
-
-			if(ret < 0) {
-				LOG_ERROR("munmap: cannot unmap page at 0x%x\n", page_addr);
-				return ret;
-			}
-
-			ret = put_page(phys_addr);
-
-			if(ret < 0) {
-				LOG_ERROR("munmap: cannot put physical page @phys 0x%x\n",
-						phys_addr);
-				return ret;
-			}
-		}
-	}
-
-	/* Indicate the area as not used anymore */
-	mmap_area_remove(viraddr);
+	/* Unmap physical pages */
+	page_unmap(viraddr, npages);
+	put_pages(phyaddr, npages);
 
 	return 0;
 }
