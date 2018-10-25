10a55b146691648660cae051e6a251e79e4f2395
Pierre Olivier <polivier@vt.edu>
2018-10-25 19:42:37 -0400

    Support mmap with different access rights

--- kernel/syscalls/mmap.c
+++ kernel/syscalls/mmap.c
@@ -6,10 +6,16 @@
 
 #define MAP_FIXED 0x10
 
+#define PROT_NONE      0
+#define PROT_READ      1
+#define PROT_WRITE     2
+#define PROT_EXEC      4
+
 size_t sys_mmap(unsigned long addr, unsigned long len, unsigned long prot,
 		unsigned long flags, unsigned long fd, unsigned long off) {
 	size_t viraddr, phyaddr, bits;
 	int err;
+	uint32_t alloc_flags;
 	uint32_t npages = PAGE_CEIL(len) >> PAGE_BITS;
 
 	if(flags & MAP_FIXED && (addr % PAGE_SIZE != 0)) {
@@ -20,35 +26,46 @@ size_t sys_mmap(unsigned long addr, unsigned long len, unsigned long prot,
 	if (BUILTIN_EXPECT(!len, 0))
 		return -EINVAL;
 
-	// get free virtual address space. We get two additional virtual pages
-	// to act at guards: they will not be mapped and will protect against
-	// overflows
+	alloc_flags = VMA_CACHEABLE;
+	if(flags & PROT_READ) alloc_flags |= VMA_READ;
+	if(flags & PROT_WRITE) alloc_flags |= VMA_WRITE;
+	if(flags & PROT_EXEC) alloc_flags |= VMA_EXECUTE;
+
+	/* get free virtual address space. We get two additional virtual pages
+	 * to act at guards: they will not be mapped and will protect against
+	 * overflows */
 	if(!addr) {
-		viraddr = vma_alloc((npages+2)*PAGE_SIZE, VMA_READ | VMA_WRITE |
-				VMA_CACHEABLE);
+		viraddr = vma_alloc((npages+2)*PAGE_SIZE, alloc_flags);
 		if (BUILTIN_EXPECT(!viraddr, 0))
 			return -ENOMEM;
 	} else {
 		viraddr = (flags & MAP_FIXED) ? addr-PAGE_SIZE : PAGE_CEIL(addr);
 		LOG_INFO("PAGE_CEIL(0x%llx) = 0x%llx\n", addr, viraddr);
-		int ret = vma_add(viraddr, viraddr+(npages+2)*PAGE_SIZE,
-				VMA_READ | VMA_WRITE | VMA_CACHEABLE);
+		int ret = vma_add(viraddr, viraddr+(npages+2)*PAGE_SIZE, alloc_flags);
+
+		/* FIXME: when the application requests an already mapped range of
+		 * virtual memory, the kernel is supposed to unmap the part that is
+		 * requested and remap it to satisfy the current mmap request. We
+		 * just fail miserably for now */
 		if(BUILTIN_EXPECT(ret, 0)) {
-			LOG_ERROR("mmap: cannot vma_add!\n");
+			LOG_ERROR("mmap: cannot vma_add, probably vma range requested is "
+					"already used\n");
 			return -EFAULT;
 		}
 	}
 
-	// get physical memory
+	/* get physical memory */
 	phyaddr = get_pages(npages);
 	if (BUILTIN_EXPECT(!phyaddr, 0)) {
 		vma_free(viraddr, viraddr+(npages+2)*PAGE_SIZE);
 		return -ENOMEM;
 	}
 
-	bits = PG_RW|PG_GLOBAL|PG_NX;
+	bits = PG_GLOBAL;
+	if(!(flags & PROT_EXEC)) bits |= PG_NX;
+	if(flags & PROT_WRITE) bits |= PG_RW;
 
-	// map physical pages to VMA
+	/* map physical pages to VMA */
 	err = page_map(viraddr+PAGE_SIZE, phyaddr, npages, bits);
 	if (BUILTIN_EXPECT(err, 0)) {
 		vma_free(viraddr, viraddr+(npages+2)*PAGE_SIZE);
