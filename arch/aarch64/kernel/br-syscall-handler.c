728cc52d1ed85a936ba217663f96486c4b4cbf53
Pierre Olivier <polivier@vt.edu>
2019-08-26 18:38:58 -0400

    Support more BR syscalls

--- arch/aarch64/kernel/br-syscall-handler.c
+++ arch/aarch64/kernel/br-syscall-handler.c
@@ -8,8 +8,10 @@ int br_syscall_handler(uint64_t x0, uint64_t x1, uint64_t x2, uint64_t x3,
     uint64_t id;
     asm volatile ("mov %0, x8" : "=r"(id) ::);
 
+#if 0
     LOG_INFO("br_syscall_handler id %d - x0: 0x%llx, x1: 0x%llx, x2: 0x%llx, "
             "x3: 0x%llx, x4: 0x%llx, x5: 0x%llx\n", id, x0, x1, x2, x3, x4, x5);
+#endif
 
     switch(id) {
         case 96:
@@ -18,12 +20,20 @@ int br_syscall_handler(uint64_t x0, uint64_t x1, uint64_t x2, uint64_t x3,
         case 29:
             return sys_ioctl(x0, x1, x2);
 
+        case 64:
+            return sys_write(x0, (void *)x1, x2);
+
         case 66:
             return sys_writev(x0, (void *)x1, x2);
 
         case 94:
             sys_exit_group(x0);
+            /* does not return */
+
+        case 173:
+            return sys_getppid();
     }
 
+    LOG_ERROR("BR_SYSCALL ID %d not supported\n", id);
     return -ENOSYS;
 }
